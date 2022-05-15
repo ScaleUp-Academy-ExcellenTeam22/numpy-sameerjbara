91d5a3fa840cd2e6173e5286940275337039a7cf
sameer jbara <sameerjbara4@gmail.com>
2022-05-16 01:04:46 +0300

    add ex17

---
+++ ex17.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def med(array: np.ndarray):
+    """
+    this function compute the median of flattened given array
+    param array: the given array
+    return: the median
+    """
+    return np.median(array)
