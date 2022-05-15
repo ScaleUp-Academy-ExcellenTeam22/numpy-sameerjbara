6ba38a68edc652ee76722e260b0a6d205a2c2cca
sameer jbara <sameerjbara4@gmail.com>
2022-05-16 01:05:10 +0300

    add ex18

---
+++ ex18.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+"""count the number of days of specific month"""
+print("Number of days, February, 2016: ")
+print(np.datetime64('2016-03-01') - np.datetime64('2016-02-01'))
+print("Number of days, February, 2017: ")
+print(np.datetime64('2017-03-01') - np.datetime64('2017-02-01'))
+print("Number of days, February, 2018: ")
+print(np.datetime64('2018-03-01') - np.datetime64('2018-02-01'))
