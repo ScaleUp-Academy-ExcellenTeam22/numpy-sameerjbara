274f562bf93a01bf58fde81de6a23d8a325efd29
sameer jbara <sameerjbara4@gmail.com>
2022-05-16 00:56:19 +0300

    ex3.py

---
+++ ex3.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def get_row_col(matrix: np.ndarray) -> tuple:
+    """
+    The function find the number of rows and columns of a given matrix.
+    @param matrix: The given matrix.
+    @return: tuple contains the row and col of the matrix.
+    """
+    return matrix.shape
