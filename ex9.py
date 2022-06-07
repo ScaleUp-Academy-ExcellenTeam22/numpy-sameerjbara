1f07758397a04b388f45da110840124197aefde1
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-06-07 17:10:06 +0300

    Update ex9.py

--- ex9.py
+++ ex9.py
@@ -3,9 +3,9 @@ import numpy as np
 
 def mul(array1: np.ndarray, array2: np.ndarray) -> np.ndarray:
     """
-    this function multiply two given arrays of same size element-by-element
+    This function multiply two given arrays of the same size element-by-element
     @param array1: the first array
     @param array2: the second array
-    return: the result of the multiply array
+    return: the result of the multiplication 
     """
     return np.multiply(array1, array2)
