75582d725bdc9deb17b04318b176a6a823b27526
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-06-07 17:14:03 +0300

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-def replace(array, num, replacement):
+def replace(array:np.array, num:int, replacement:int):
     """
-    this function replace all numbers in a given array which is equal, less and greater to a given number
-    @param array : the given array 
+    This function replace all values in the given array that is equal, less and greater than the given number
+    @param array : the given array
     @param num : the compared number
-    @param replacement : the replacement number 
+    @param replacement : the replacement number
     """
     print(np.where(array == num, replacement, array))
     print(np.where(array < num, replacement, array))
