b5b9bfb30073cd32d1c38689293312d17e40bf89
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-06-07 17:06:14 +0300

    Update ex12.py

--- ex12.py
+++ ex12.py
@@ -1,5 +1,11 @@
 import numpy as np
 
 """ remove single-dimensional entries from a specified shape """
+
+
+def reshape(array: np.array) -> np.ndarray:
+    print(np.squeeze(array))
+
+
 array = np.zeros((3, 1, 4))
-print(np.squeeze(array).shape)
+reshape(array)
