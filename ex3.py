153ec5eae06b4b5c875a4913317f4b94a588af37
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-06-07 17:37:13 +0300

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 
-def get_row_col(matrix: np.ndarray) -> tuple:
+def get_matrix_size(matrix: np.ndarray) -> tuple:
     """
     The function find the number of rows and columns of a given matrix.
     @param matrix: The given matrix.
-    @return: tuple contains the row and col of the matrix.
+    @return: tuple that contains the size of the matrix.
     """
     return matrix.shape
