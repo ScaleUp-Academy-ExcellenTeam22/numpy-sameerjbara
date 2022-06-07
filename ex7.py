9a89696c6fc265277a3c009b213933a98ea52307
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-06-07 17:22:35 +0300

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,5 +1,11 @@
 import numpy as np
 
-array = np.empty((4, 4), np.int64)
-swapped_array = array[::-1]
 
+def arrayinit() -> np.ndarray:
+    array = np.empty((4, 4), np.int64)
+    swapped_array = array[::-1]
+
+    return swapped_array
+
+
+arrayinit()
